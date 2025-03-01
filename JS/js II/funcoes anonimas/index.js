b246f858fb00c96536f111c14d9e0c79574a217c
Gabriel Araujo <araujogabrielhn@gmail.com>
2025-02-28 22:37:15 -0300

    Arquivos

---
+++ JS/js II/funcoes anonimas/index.js
@@ -0,0 +1,27 @@
+function somar (a, b) {
+    return a + b
+}
+
+let operacao = somar 
+
+console.log(operacao (4, 5))
+
+operacao = function (a, b) {
+    return a - b
+}
+
+console.log(operacao (4, 5))
+
+
+olaMundo()
+
+
+function olaMundo() {
+    console.log("olá, mundo!")
+}
+
+const oiMundo = function () {
+    console.log("oi, mundo")
+}
+
+oiMundo()
---
+++ JS/js II/high order functions/index.js
@@ -0,0 +1,6 @@
+function calcular(a, b, operacao) {
+    console.log("Realizando uma operacao.")
+    const resultado = operacao (a, b)
+    return resultado
+}
+
