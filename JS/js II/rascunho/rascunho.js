998973395976c758192326c914506deabcb0210b
Gabriel Araujo <araujogabrielhn@gmail.com>
2025-03-10 21:19:28 -0300

    Arquivos

--- JS/js II/rascunho/rascunho.js
+++ JS/js II/rascunho/rascunho.js
@@ -1,6 +1,18 @@
-console.log(nome)
+const turista = prompt("Qual o seu nome?")
+let cidades = ""
+let contagem = 0
 
-var nome = "Gab"
-let sobrenome = "Araujo"
+let continuar = prompt("você visitou alguma cidade?")
 
-console.log(nome)
+while (continuar === "Sim") {
+    let cidade = prompt("qual é o nome da cidade visitada?")
+    cidades += " - " + cidade + "\n"
+    contagem++
+    continuar = prompt("Você visitou alguma outra cidade?")
+}
+
+alert(
+    "Turista: " + turista +
+    "\nQuantidade de cidades visitadas: " + contagem +
+    "\nCidades visitadas:\n" + cidades
+)
---
+++ JS/js lll/Manipulando atributos com JS/index.js
@@ -0,0 +1,12 @@
+const input = document.getElementById('input')
+
+document.getElementById('value').addEventListener('click', function(){
+    input.value = 'Olá, mundo!'
+
+    console.log(input.value)
+    console.log(input.getAttribute('value'))
+})
+
+document.getElementById('type').addEventListener('click', function(){
+    input.type = input.type !== 'radio' ? 'radio' : 'text'
+})
