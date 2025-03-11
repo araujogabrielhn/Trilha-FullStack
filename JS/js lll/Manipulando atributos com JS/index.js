b5c97bcf81f53b36dba60bacbf47d3c295ea965b
Gabriel Araujo <araujogabrielhn@gmail.com>
2025-03-11 15:02:57 -0300

    Arquivos

--- JS/js lll/Manipulando atributos com JS/index.js
+++ JS/js lll/Manipulando atributos com JS/index.js
@@ -8,5 +8,21 @@ document.getElementById('value').addEventListener('click', function(){
 })
 
 document.getElementById('type').addEventListener('click', function(){
-    input.type = input.type !== 'radio' ? 'radio' : 'text'
+   // input.type = input.type !== 'radio' ? 'radio' : 'text'
+   input.setAttribute('type', 'radio')
+})
+
+document.getElementById('placeholder').addEventListener('click', function(){
+    input.placeholder = 'digita algo...'
+})
+
+document.getElementById('disable').addEventListener('click', function*(){
+    input.setAttribute('disable', !input.disabled)
+})
+
+document.getElementById('data').addEventListener('click', function(){
+    const data = input.dataset.somethingElse
+    console.log("o valor do atributo data-something é: " + data)
+    input.dataset.something = 'algum outro valor'
+    console.log("o valor do atributo data-something agora é: " + input.dataset.somethingElse)
 })
