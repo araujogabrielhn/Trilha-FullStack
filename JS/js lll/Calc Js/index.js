1424023a2b87c958fc886a410c81eba0276e0ca0
Gabriel Araujo <araujogabrielhn@gmail.com>
2025-03-31 13:43:56 -0300

    arquivos

--- JS/js lll/Calc Js/index.js
+++ JS/js lll/Calc Js/index.js
@@ -35,7 +35,9 @@ document.getElementById("equal").addEventListener("click", calculate)
 function calculate() {
   resultInput.value = "ERROR"
   resultInput.classList.add("error")
+  
   const result = eval(input.value)
+  
   resultInput.value = result
   resultInput.classList.remove("error")
 }
