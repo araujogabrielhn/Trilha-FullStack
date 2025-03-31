42caff17c618d4f772da57353e50604ea5b34310
Gabriel Araujo <araujogabrielhn@gmail.com>
2025-03-31 13:04:35 -0300

    arquivos

--- JS/js IV/tipos-de-export-esm/index.js
+++ JS/js IV/tipos-de-export-esm/index.js
@@ -1,8 +1,10 @@
 import { inline } from "./inline.js"
 import defaultInline from "./inline.js"
+import { group } from "./non-inline.js"
+import exportDefault from "./non-inline.js"
 
 inline ()
 defaultInline ()
 
 group ()
-nonInline ()
+exportDefault ()
