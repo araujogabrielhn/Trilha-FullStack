11524eaac563b7ef19897bc05cb4898a3f65d0d5
Gabriel Araujo <araujogabrielhn@gmail.com>
2025-04-01 12:33:02 -0300

    arquivos

---
+++ JS/js IV/datas-com-dayjs/birthday.js
@@ -0,0 +1,16 @@
+const dayjs = require("dayjs")
+
+function birthday(date) {
+    const birthday = dayjs(date)
+    const today = dayjs()
+
+    const ageInYears = today.diff(birthday, 'year')
+    const nextBirthday = birthday.add(ageInYears + 1, 'year')
+    const daysToNextBirthday = nextBirthday.diff(today, 'day')
+
+    console.log(`Idade: ${ageInYears}`)
+    console.log(`Proximo aniversário: ${nextBirthday.format("DD/MM/YYYY")}`)
+    console.log(`Dias até o proximo aniversario ${ageInYears + 1} anos: ${daysToNextBirthday}`)
+}
+
+birthday("1997-08-08")
