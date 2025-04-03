ffacf0c41fe41e18769c332a1660991e57adbe52
Gabriel Araujo <araujogabrielhn@gmail.com>
2025-04-03 12:01:24 -0300

    03.04

--- JS/js IV/usando-o-webpack/webpack.config.js
+++ JS/js IV/usando-o-webpack/webpack.config.js
@@ -1,6 +1,14 @@
+const path = require('path')
+
 module.exports = {
     entry: {
-        index: '.src/index.js'
+        main: './src/index.js',
+        hello: './src/hello.js'
+
     },
-    mode: 'development'
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.min.js'
+  }
 }
