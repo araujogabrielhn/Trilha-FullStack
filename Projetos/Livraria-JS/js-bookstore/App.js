267f3a79c5da72360d1b64bd17fb2938c0110614
Gabriel Araujo <araujogabrielhn@gmail.com>
2025-04-16 14:22:19 -0300

    está sofrido pacas

---
+++ Projetos/Livraria-JS/js-bookstore/App.js
@@ -0,0 +1,75 @@
+const Database = require("./Database")
+const Order = require("./entities/Order")
+const Poster = require("./entities/Poster")
+const Product = require("./entities/Product")
+const User = require("./entities/User")
+const Author = require("./entities/Author")
+const Book = require("./entities/Book")
+
+module.exports = class App {
+    static #database = new Database()
+
+    createUser(name, email, password) {
+        const user = new User(name, email, password)
+        App.#database.saveUser(user)
+    }
+
+    getUsers() {
+        App.#database.find('users')
+    }
+
+    createAuthor(name, nationality, bio) {
+        const author = new Author(name, nationality, bio)
+        App.#database.saveAuthor(author)
+    }
+
+    getAuthors() {
+        return App.#database.find('authors')
+    }
+
+    createBook(title, synopsis, genre, pages, author, description, price, inStock) {
+        const book = new Book(title, synopsis, genre, pages, author, description, price, inStock)
+        App.#database.saveBook(book)
+    }
+
+    addBook(bookName, quantity) {
+        App.#database.addBookstoStock(bookName, quantity)
+    }
+
+    getBooks(){
+        return App.#database.find('books')
+    }
+
+    createPoster(name, description, height, width, price, inStock) {
+        const poster = new Poster(name, description, height, width, price, inStock)
+        App.#database.savePoster(poster)
+    }
+
+    addPoster(posterName, quantity) {
+        App.#database.addPosterstoStock(posterName, quantity)
+    }
+
+    getPosters() {
+        return App.#database.find('posters')
+    }
+
+    createOrder(items, user) {
+        const order = new Order(items, user)
+        App.#database.saveOrder(order)
+        order.data.items.forEach(({ product, quantity }) => {
+            if(product instanceof Book) {
+                App.#database.removeBooksFromStock(product.name, quantity)
+            } else if (product instanceof Poster) {
+                App.#database.removePostersFromStock(product.name, quantity)
+            }
+        })
+    }
+
+    getOrders() {
+        return App.#database.find('orders')
+    }
+
+    showDatabase() {
+        App.#database.showStorage()
+    }
+}
---
+++ Projetos/Livraria-JS/js-bookstore/Database.js
@@ -0,0 +1,79 @@
+module.exports = class Database { 
+    #storage = {
+        authors: [],
+        books: [],
+        posters: [],
+        orders: [],
+        users: []
+    }
+
+    find(key) {
+        return this.#storage[key]
+    }
+
+    saveAuthor(author) {
+        this.#storage.authors.push(author)
+    }
+
+    findBookByName(bookName) {
+        return this.#storage.books.find(b => b.name === bookName)
+    }
+
+    saveBook(book) {
+        const bookExists = this.findBookByname(book.name)
+        if (!bookExists) {
+            this.#storage.books.push(book)
+        }
+    }
+
+    addBookstoStock(bookName, quantity) {
+        const book = this.findBookByname(bookName)
+        book?.addToStock(quantity)
+    }
+
+    removeBooksFromStock(bookName, quantity) {
+        const book = this.findBookByname(bookName)
+        book?.removebooksFromStock
+    }
+
+    findPosterByName(posterName) {
+        return this.#storage.posters.find(p => p.name === bookName)
+    }
+
+    savePoster(poster) {
+        const posterExists = this.findposterByname(poster.name)
+        if (!posterExists) {
+            this.#storage.posters.push(book)
+        }
+    }
+
+    addPosterstoStock(posterName, quantity) {
+        const poster = this.findposterByname(posterName)
+        poster?.addToStock(quantity)
+    }
+
+    removePostersFromStock(posterName, quantity) {
+        const book = this.findPosterByname(posterName)
+        poster?.removePostersFromStock
+    }
+
+    saveUser(user) {
+        const userExists = this.#storage.users.find(u => u.email === user)
+        if (!userExists) {
+            this.#storage.users.push(user)
+        }
+    }
+
+    saveOrder (order) {
+        this.#storage.orders.push()
+    }
+
+    showStorage() {
+        console.table(this.#storage.authors)
+        console.table(this.#storage.books)
+        console.table(this.#storage.posters)
+        console.table(this.#storage.user)
+        console.table(this.#storage.orders.map(order => order.data))
+
+    }
+}
